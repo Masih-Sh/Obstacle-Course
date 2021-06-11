1a51913dc992a0837e7f6d54d7e82e0d7e6eaa77
Masih Shafieian <masih.shafieian@gmail.com>
2021-06-11 17:11:30 +0100

    camera and walls added

--- Obstacle Course/Assets/Mover.cs
+++ Obstacle Course/Assets/Mover.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField]float moveSpeed = 1.0f;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xValue = Input.GetAxis("Horizontal")*Time.deltaTime*moveSpeed;
+        float zValue = Input.GetAxis("Vertical")*Time.deltaTime*moveSpeed;
+
+        transform.Translate(xValue,0,zValue);
     }
 }
